8888cbd7b2bd2177a53ffe81c5eb0c8db22f7390
Pedro Marques <pedromarques391@gmail.com>
2026-05-09 13:44:03 -0300

    feat: create method to check_divergences and edit method that normalizes values, now normalize the values of csv file.

--- src/processing.py
+++ src/processing.py
@@ -20,18 +20,38 @@ class Processing:
         return name
     
     @staticmethod
-    def normalize_value(value: int) -> str:
-        intToFloat = round(float(value), 2)
-        normalized_value = f"{intToFloat:.2f}".replace('.', ',')
-        return normalized_value
+    def normalize_value_csv_to_float(value) -> float:
+        if pd.isna(value):
+            return 0.0
+        if isinstance(value, str):
+            value = value.replace('.', '').replace(',', '.')
+            return float(value)
+        return float(value)
             
     
     @staticmethod
     def merge_dataFrames(df1: pd.DataFrame, df2: pd.DataFrame, key: str) -> pd.DataFrame:
-        merged_df = pd.merge(df1, df2, on=key, how="outer", indicator=True, suffixes=('_excel', '_csv'))
+        merged_df = pd.merge(df1, df2, on=key, how="outer", indicator=True)
         return merged_df
     
     @staticmethod
     def rename_columns(df: pd.DataFrame, columns: dict) -> pd.DataFrame:
         return df.rename(columns=columns)
+    
+    @staticmethod
+    def check_divergences(row: pd.Series) -> str :
+        notes = []
+        if row['_merge'] == 'right_only':
+            notes.append("Não encontrado no sistema do convênio")
+        elif row['_merge'] == 'left_only':
+            notes.append("Não lançado na planilha interna")
+        elif row['_merge'] == 'both':
+            if float(row['vl_liquido']) != float(row['valor']):
+                notes.append(f"Divergência de valor: CSV {row['vl_liquido']} vs Excel {row['valor']}")
+            if row['nome_beneficiario'] != row['paciente']:
+                notes.append("Nome do paciente com erro de digitação")
+            if str(row['ans']) != str(row['registro_ans']):
+                notes.append("Divergência no código de convênio")
+                
+        return " | ".join(notes)
     
