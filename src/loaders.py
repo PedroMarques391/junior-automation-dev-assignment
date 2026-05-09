127d1a3c4c44f96824d8833ca29b22d1c8a3d352
Pedro Marques <pedromarques391@gmail.com>
2026-05-09 14:24:18 -0300

    feat: add logs directly in the class methods.

--- src/loaders.py
+++ src/loaders.py
@@ -1,11 +1,17 @@
+import logging
 import pandas as pd
 
+logger = logging.getLogger(__name__)
 
 class DataLoader:
     
     @staticmethod
     def load_data(file_path: str) -> pd.DataFrame:
+        logger.info(f"Carregando dados de: {file_path}")
         if file_path.endswith('.csv'):
-            return pd.read_csv(file_path, sep=';')
-        
-        return pd.read_excel(file_path)
+            df = pd.read_csv(file_path, sep=';')
+        else:
+            df = pd.read_excel(file_path)
+            
+        logger.info(f"Dados carregados com sucesso de {file_path}. Total de linhas: {len(df)}")
+        return df
--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,9 @@
-from unidecode import unidecode
+import logging
 import re
 import pandas as pd
+from unidecode import unidecode
 
+logger = logging.getLogger(__name__)
 
 class Processing:
     @staticmethod
@@ -31,11 +33,13 @@ class Processing:
     
     @staticmethod
     def merge_dataFrames(df1: pd.DataFrame, df2: pd.DataFrame, key: str) -> pd.DataFrame:
+        logger.info(f"Iniciando o cruzamento dos dados (merge) utilizando a chave '{key}'...")
         merged_df = pd.merge(df1, df2, on=key, how="outer", indicator=True)
         return merged_df
     
     @staticmethod
     def rename_columns(df: pd.DataFrame, columns: dict) -> pd.DataFrame:
+        logger.info(f"Renomeando colunas: {columns}")
         return df.rename(columns=columns)
     
     @staticmethod
@@ -43,15 +47,21 @@ class Processing:
         notes = []
         if row['_merge'] == 'right_only':
             notes.append("Não encontrado no sistema do convênio")
+            logger.error(f"Não encontrado no sistema do convênio: {row}")
         elif row['_merge'] == 'left_only':
             notes.append("Não lançado na planilha interna")
+            logger.error(f"Não lançado na planilha interna: {row}")
         elif row['_merge'] == 'both':
             if float(row['vl_liquido']) != float(row['valor']):
                 notes.append(f"Divergência de valor: CSV {row['vl_liquido']} vs Excel {row['valor']}")
+                logger.warning(f"Divergência de valor: CSV {row['vl_liquido']} vs Excel {row['valor']}")
+                logger.warning(f"Diferença: {float(row['vl_liquido']) - float(row['valor'])}")
             if row['nome_beneficiario'] != row['paciente']:
                 notes.append("Nome do paciente com erro de digitação")
+                logger.warning(f"Nome do paciente com erro de digitação: {row['nome_beneficiario']} vs {row['paciente']}")
             if str(row['ans']) != str(row['registro_ans']):
                 notes.append("Divergência no código de convênio")
+                logger.warning(f"Divergência no código de convênio: {row['ans']} vs {row['registro_ans']}")
                 
         return " | ".join(notes)
     
