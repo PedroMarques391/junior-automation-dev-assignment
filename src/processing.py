c3380516a4eaf8535e2fd53027bcebcd3a5d4fbc
Pedro Marques <pedromarques391@gmail.com>
2026-05-09 10:18:58 -0300

    feat: create method to normalize collumns name and merge data frames.

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,6 @@
 from unidecode import unidecode
 import re
+import pandas as pd
 
 
 class Processing:
@@ -17,3 +18,12 @@ class Processing:
         name = re.sub(r'\s+', ' ', name)
     
         return name
+    
+    @staticmethod
+    def merge_dataFrames(df1: pd.DataFrame, df2: pd.DataFrame, key1: str, key2: str) -> pd.DataFrame:
+        merged_df = pd.merge(df1, df2, left_on=key1, right_on=key2, how='outer')
+        return merged_df
+    
+    @staticmethod
+    def rename_columns(df: pd.DataFrame, columns_mapping: dict) -> pd.DataFrame:
+        return df.rename(columns=columns_mapping)
