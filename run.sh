fe81aabe89851729c6c7b15a6012e7ff6ca5c087
Pedro Marques <pedromarques391@gmail.com>
2026-05-10 23:23:13 -0300

    script: add cronjob for Monday at 6:30 am.

--- run.sh
+++ run.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
-# Verificando se o Python está instalado
 
 if ! command -v python &> /dev/null; then
     echo "Python não encontrado" >> "$LOG_FILE"
     exit 1
 fi
 
-# Verificando se o pip está instalado
 
 if ! command -v pip &> /dev/null; then
     echo "Pip não encontrado" >> "$LOG_FILE"
     exit 1
 fi
 
-# Configurações iniciais
 
 PROJECT_DIR="$(cd "$(dirname "$0")" && pwd)"
 LOG_FILE="$PROJECT_DIR/logs/shell/execution_$(date +%Y-%m-%d_%H-%M-%S).log"
 PROGRAM_LOG_FILE="$PROJECT_DIR/logs/"
 VENV_PATH="$PROJECT_DIR/venv/bin/activate"
 
-# Cria o diretório de logs se não existir
-
 mkdir -p "$(dirname "$LOG_FILE")"
 
 echo "Iniciando Pipeline: $(date '+%Y-%m-%d %H:%M:%S')" >> "$LOG_FILE"
 
-# Ativando o ambiente virtual
 
 if [ -f "$VENV_PATH" ]; then
     source "$VENV_PATH"
@@ -38,7 +32,6 @@ else
     source .venv/bin/activate
 fi
 
-# Descompactando os arquivos zipados
 
 if [ -d "/data/" ]; then
     echo "Os arquivos já estão descompactados"
@@ -47,7 +40,6 @@ else
     echo "Os arquivos foram descompactados com sucesso"
 fi
 
-## Instalando as dependências
 
 if [ -f "$PROJECT_DIR/requirements.txt" ]; then
     pip install -r "$PROJECT_DIR/requirements.txt" >> "$LOG_FILE" 2>&1
@@ -57,11 +49,9 @@ else
     exit 1
 fi
 
-## Executando o Script
 
 python3 "$PROJECT_DIR/main.py" >> "$LOG_FILE" 2>&1
 
-# Verificando o código de saída
 
 if [ $? -eq 0 ]; then
     echo "[SUCCESS] Pipeline finalizado com sucesso em $(date '+%H:%M:%S')" >> "$LOG_FILE"
@@ -71,3 +61,6 @@ else
     echo "[CRITICAL] Falha na execução do pipeline em $(date '+%H:%M:%S')" >> "$LOG_FILE"
     exit 1
 fi
+
+
+# 30 06 * * 1 /bin/bash $PROJECT_DIR/run.sh
